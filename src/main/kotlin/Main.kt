963cd8b4d09dda1c5e07d8f9b055c56c8c6731e8
Idayam <mzeeidaya@gmail.com>
2023-02-03 08:27:32 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+    val fullname: String="Amina Swaleh"
+    var age: Byte=26
+    var phoneNo: Int = 715368487
+    var weight: Byte = 46
+    val kenya: Boolean = true
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
